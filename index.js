f268daf1fb9a4b2cc10a7ff14640be5a5a65e884
Saksham-Xtreme <shivbhau2108@gmail.com>
2026-02-01 17:22:32 +0530

    Add index.js file

--- index.js
+++ index.js
@@ -1,108 +1,81 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path =  require("path");
+const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
-
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-
-main()
-    .then(() => {
-        console.log("connection successful");
-    })
-    .catch(err => console.log(err));
-
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/desiWhatsapp");
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected");
 }
+main().catch(err => console.log(err));
 
-
-// let chat1 = new Chat({
-//     from: "Saksham",
-//     to : "Muskan",
-//     msg : "I will be successful",
-//     created_at : new Date()
-// });
-
-// chat1.save().then((res) => {
-//     console.log(res);
-// });
-
-
-// Index JS
+// INDEX
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-   
-    res.render("index.ejs", {chats});
+    res.render("index.ejs", { chats });
 });
 
-// New route
+// NEW
 app.get("/chats/new", (req, res) => {
     res.render("new.ejs");
 });
 
-// Create route
-app.post("/chats", (req, res) => {
-    let {from, to , msg} = req.body;
+// CREATE
+app.post("/chats", async (req, res) => {
+    let { from, to, msg } = req.body;
 
-    let newChat = new Chat({
-        from:from,
-        to:to,
-        msg:msg,
-        created_at : new Date()
+    await Chat.create({
+        from,
+        to,
+        msg,
+        created_at: new Date()
     });
 
-    newChat
-    .save()
-    .then(res => {
-        console.log("Chat is saved")
-    })
-    .catch(err => 
-        {console.log(err)}
-    );
     res.redirect("/chats");
+});
 
-})
-
-// edit route
+// EDIT FORM
 app.get("/chats/:id/edit", async (req, res) => {
-    let {id} = req.params;
-    let chat =  await Chat.findById(id);
-    res.render("edit.ejs", {chat});
+    let chat = await Chat.findById(req.params.id);
+    if (!chat) return res.status(404).send("Chat not found");
+    res.render("edit.ejs", { chat });
 });
 
-//Post the edit route
-
+// UPDATE
 app.put("/chats/:id", async (req, res) => {
-    let {id} = req.params;
-    let {msg: newMsg} = req.body;
+    let { msg } = req.body;
 
-    let updatedChat = await Chat.findByIdAndUpdate(id, {msg : newMsg}, {runValidator: true, new: true});
-    console.log(updatedChat);
+    await Chat.findByIdAndUpdate(
+        req.params.id,
+        { msg },
+        { runValidators: true }
+    );
+
     res.redirect("/chats");
-
 });
 
-// delete route
-app.delete("/chats/:id", async(req, res) => {
-    let {id} = req.params;
-    let deletedchat = await Chat.findByIdAndDelete(id);
-    console.log(deletedchat);
+// DELETE
+app.delete("/chats/:id", async (req, res) => {
+    await Chat.findByIdAndDelete(req.params.id);
     res.redirect("/chats");
-
 });
 
-app.get("/" , (req, res) => {
+app.get("/", (req, res) => {
     res.send("Working well");
 });
 
-app.listen(8080, () => {
-    console.log("Server is listing");
-}); 
+// ✅ PORT FIX
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
